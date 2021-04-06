aeaf01bf72cc906899d70009b45bc91b8a446538
Martin Kim <martinjwkim@gmail.com>
2021-04-06 02:58:07 -0700

    remove flipped function

--- src/Card.js
+++ src/Card.js
@@ -1,37 +1,20 @@
 import React, { useState } from 'react';
 import { Box } from 'grommet';
-import ReactCardFlip from 'react-card-flip'
-import CardFront from './CardBack'
-import CardBack from './CardFront'
 
 const Card = ({ title }) => {
-
-  const [flipped, setFlipped] = useState(true)
 
   const props = {
     main: {
       fill: true,
       align: 'center',
       justify: 'center',
-      onMouseOver:()=>setFlipped(false),
-      onMouseLeave:()=>setFlipped(true), 
-    },
-    card: {
-      isFlipped: flipped,
-      flipDirection: 'horizontal',
-      containerStyle: {
-        width: '100%',
-        height: '100%',
-      }
+      background: 'brand',
     },
   }
 
   return (
     <Box {...props.main}>
-      <ReactCardFlip {...props.card}>
-        <CardFront />
-        <CardBack />
-      </ReactCardFlip>
+      {title}
     </Box >
   )
 }
