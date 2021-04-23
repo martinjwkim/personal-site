4601df5eb074fe931d39388669e98e3f8ed1154c
Martin Kim <martinjwkim@gmail.com>
2021-04-22 23:56:23 -0700

    nature background

--- src/Background.js
+++ src/Background.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import background from './imac-cropped.png'
+import natureBackground from './nature-background.jpg'
 
 const Background = ({ animationType }) => {
 
   return (
     <div className='Background'>
-      <img src={background} alt='background' className={animationType} />
+      <img src={background} alt='background' className={`computer-background ${animationType}`} />
+      <img src={natureBackground} alt='nature-background' className='nature-background'/>
     </div>
   )
 }
