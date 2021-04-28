6df10efa646684b97e1dfc9c872ff6208a985c57
Martin Kim <martinjwkim@gmail.com>
2021-04-28 07:56:02 -0700

    frame with scroll listener

--- src/About/About.js
+++ src/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from 'grommet';
 import Portrait from './Portrait'
 import Bio from './Bio'
-
+import frame from '../images/frame.png'
 
 const About = ({ size }) => {
 
@@ -17,11 +17,13 @@ const About = ({ size }) => {
       width: { max: '1200px' },
       alignSelf: 'center',
       gap: 'medium',
+      className: 'About'
     },
   }
 
   return (
     <Box {...props.main}>
+      <img src={frame} alt='frame' className='frame' />
       <Portrait size={size} />
       <Bio size={size} />
     </Box >
--- src/App.js
+++ src/App.js
@@ -33,7 +33,8 @@ function App() {
     grommetRef.current.addEventListener('scroll', () => {
       const scroll = grommetRef.current.scrollTop;
       $('.coffee').css("transform", `translate(50%, 50%) scale(0.5, 0.5) rotate(${-90*scroll/800}deg)`);
-      $('.paper').css('transform', `scale(0.35, 0.3) translate(0, ${2*(4800-h-scroll)}px) rotate(${45*(4800-h-scroll)/h}deg)`)
+      $('.paper').css('transform', `scale(0.35, 0.3) translate(0, ${2*(4800-h-scroll)}px) rotate(${45*(4800-h-scroll)/h}deg)`);
+      $('.frame').css("transform", `translate(-${(0.01*(1152-h/2-scroll))**2}%,${(0.05*(1152-h/2-scroll))}%)`);
     })
   },[h])
 
