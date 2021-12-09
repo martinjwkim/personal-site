e36705a6a23be7ecf253a0a910bb8531809e6c96
Martin Kim <martinjwkim@gmail.com>
2021-12-09 13:39:21 -0800

    change portrait photo

--- src/About/Portrait.js
+++ src/About/Portrait.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { Box, Image } from 'grommet';
-import { useInView } from 'react-hook-inview'
-import frame from '../images/frame.png'
+import React from "react";
+import { Box, Image } from "grommet";
+import { useInView } from "react-hook-inview";
+import frame from "../images/frame.png";
 
 const Portrait = ({ size }) => {
-
   const [ref, inView] = useInView({
     threshold: 0,
     unobserveOnEnter: true,
@@ -12,32 +11,32 @@ const Portrait = ({ size }) => {
 
   const props = {
     main: {
-      overflow: 'visible',
-      height: size === 'small' ? 'small' : { max: 'medium' },
-      width: size === 'small' ? 'small' : { max: 'medium' },
-      fill: size==='small'? false : true,
+      overflow: "visible",
+      height: size === "small" ? "small" : { max: "medium" },
+      width: size === "small" ? "small" : { max: "medium" },
+      fill: size === "small" ? false : true,
       ref,
       animation: {
-        type: inView ? 'fadeIn' : 'fadeOut',
+        type: inView ? "fadeIn" : "fadeOut",
         delay: 100,
         duration: 4000,
-        size: 'medium'
+        size: "medium",
       },
-      className: 'Portrait'
+      className: "Portrait",
     },
     image: {
       fit: "cover",
-      src: "/portrait.png",
-      className: 'ProfilePic'
-    }
-  }
+      src: "/portrait-2.jpg",
+      className: "ProfilePic",
+    },
+  };
 
   return (
     <Box {...props.main}>
-      {size!=='small' && <img src={frame} alt='frame' className='frame' />}
+      {size !== "small" && <img src={frame} alt="frame" className="frame" />}
       <Image {...props.image} />
     </Box>
-  )
-}
+  );
+};
 
 export default Portrait;
